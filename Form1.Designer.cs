b5c14dcb3400ede30c180b5dc61d0d387ca0fcd0
Pieter Claassens <pieterc@shockeos.com>
2025-10-21 20:56:08 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FileCopyTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.btnTestNetworkDrive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Location = new System.Drawing.Point(0, 0);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(93, 23);
+            this.btnTest1.TabIndex = 0;
+            this.btnTest1.Text = "test1_local";
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(12, 239);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(58, 13);
+            this.lblResults.TabIndex = 1;
+            this.lblResults.Text = "Status: .....";
+            // 
+            // btnTestNetworkDrive
+            // 
+            this.btnTestNetworkDrive.Location = new System.Drawing.Point(0, 29);
+            this.btnTestNetworkDrive.Name = "btnTestNetworkDrive";
+            this.btnTestNetworkDrive.Size = new System.Drawing.Size(93, 23);
+            this.btnTestNetworkDrive.TabIndex = 2;
+            this.btnTestNetworkDrive.Text = "test2_remote";
+            this.btnTestNetworkDrive.Click += new System.EventHandler(this.btnTestNetworkDrive_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnTestNetworkDrive);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnTest1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+      
+        private System.Windows.Forms.Button btnTest1;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Button btnTestNetworkDrive;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCopyTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest1_Click(object sender, EventArgs e)
+        {
+            lblResults.Text = "Starting test1...";
+            string fileToCopy = "c:\\tmp\\from1.txt";
+            string destinationDirectory = "c:\\log\\from1.txt";
+
+            File.Copy(fileToCopy, destinationDirectory, true);
+            lblResults.Text = "Done - Copied locally";
+        }
+
+        private void btnTestNetworkDrive_Click(object sender, EventArgs e)
+        {
+            lblResults.Text = "Starting test2...";
+            string fileToCopy = "c:\\tmp\\from1.txt";
+            string destinationDirectory = "\\\\BeeStation\\home\\Files\\shock\\from2.txt";
+
+            File.Copy(fileToCopy, destinationDirectory, true);
+            lblResults.Text = "Done - Copied remotely";
+        }
+    }
+}
